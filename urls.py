f7a67c251fba4ac3de5f3fdba9393e2da44f85a5
Sergei Lebedev <superbobry@gmail.com>
2011-07-30 12:49:43 +0400

    Static is served by Django for now

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@
 
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
@@ -9,4 +10,4 @@ admin.autodiscover()
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {"template": "index.html"}),
     url(r"^admin/", include(admin.site.urls)),
-)
+) + staticfiles_urlpatterns()
