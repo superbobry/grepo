1471b8cd4e69f854e729b4b0f4e009e9062f2dd4
Sergei Lebedev <superbobry@gmail.com>
2011-07-31 01:55:19 +0400

    Updated Repository admin -- looks a bit nicer now

--- apps/grepo_base/admin.py
+++ apps/grepo_base/admin.py
@@ -3,9 +3,28 @@
 from __future__ import absolute_import
 
 from django.contrib import admin
+from django.utils.translation import ugettext as _
+from django.template.defaultfilters import urlize
 
 from .models import Language, Repository
 
 
-admin.site.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    list_filter = ["languages", "source", "updated_at"]
+    list_display = ["name", "url_with_link", "all_languages", "score",
+                    "updated_at", "source"]
+    list_per_page = 25
+    list_select_related = True
+
+    def url_with_link(self, instance):
+        return urlize(instance.url)
+    url_with_link.short_description = _("URL")
+    url_with_link.allow_tags = True
+
+    def all_languages(self, instance):
+        return ", ".join(map(unicode, instance.languages.all()))
+    all_languages.short_description = _("languages")
+
+
+admin.site.register(Repository, RepositoryAdmin)
 admin.site.register(Language)
