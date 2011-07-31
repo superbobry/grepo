633b272cbb045df60b850330fb8a73bbc0cc8376
Fedor Gogolev <knsd@knsd.net>
2011-08-01 02:16:09 +0400

    Support for github specify language names

--- apps/grepo_base/backends/github.py
+++ apps/grepo_base/backends/github.py
@@ -21,6 +21,27 @@ from grepo_base.models import Language
 SEARCH_PATH = "/api/v2/json/repos/search/language:{lang}?start_page={page}"
 #: Github host, used for httplib for connect to api
 GITHUB = "github.com"
+#: Github specify language names
+GITHUB_LANGS = {
+    'Actionscript': 'ActionScript',
+    'Applescript': 'AppleScript',
+    'Asp': 'ASP',
+    'Coffeescript': 'CoffeeScript',
+    'Coldfusion': 'ColdFusion',
+    'Lisp': ['"Common Lisp"', '"Emacs Lisp"'],
+    'Fortran': 'FORTRAN',
+    'Javascript': 'JavaScript',
+    'Llvm': 'LLVM',
+    'Ocaml': 'OCaml',
+    'Ooc': 'ooc',
+    'Php': 'PHP',
+    'Visual Basic': '"Visual Basic"',
+}
+
+GREPO_LANGS = {
+    "Common Lisp": "Lisp",
+    "Emacs Lisp": "Lisp"
+}
 
 
 parse_gh_datetime = parser.parse
@@ -58,36 +79,49 @@ class GithubBackend(object):
             math.exp(issues / (watchers + forks))
         return score % 365. / 365.
 
+    def iter_for_lang(self, lang):
+        for page in itertools.count(1):
+            self.fetch(lang, page)
+            repositories = json.loads(self.data)["repositories"]
+
+            if not repositories:
+                break
+
+            for repository in repositories:
+                if (not repository["language"] or
+                    repository["fork"] or
+                    repository["private"]):
+                    continue
+
+                # If there is no "updated_at" field in api output,
+                # then repository wasn't ever updated and `updated_at`
+                # equals to `created_at`
+                updated = repository.get("updated_at",
+                                         repository["created_at"])
+                updated = parse_gh_datetime(updated)
+                created = parse_gh_datetime(repository["created_at"])
+
+                language = GREPO_LANGS.get(repository["language"].title(),
+                                           repository["language"].title())
+                languages = [language]
+                yield {
+                    "url": repository["url"],
+                    "name": repository["name"],
+                    "languages": languages,
+                    "score": self.calculate_score(updated, repository),
+                    "summary": repository.get("description", ""),
+                    "updated_at": updated,
+                    "created_at": created
+                }
+
     def __iter__(self):
         """Yields all repositories one by one."""
         for language in Language.objects.all():
-            for page in itertools.count(1):
-                self.fetch(language.name, page)
-                repositories = json.loads(self.data)["repositories"]
+            langs = GITHUB_LANGS.get(language.name, language.name)
 
-                if not repositories:
-                    break
+            if not isinstance(langs, list):
+                langs = [langs]
 
-                for repository in repositories:
-                    if (not repository["language"] or
-                        repository["fork"] or
-                        repository["private"]):
-                        continue
-
-                    # If there is no "updated_at" field in api output,
-                    # then repository wasn't ever updated and `updated_at`
-                    # equals to `created_at`
-                    updated = repository.get("updated_at",
-                                             repository["created_at"])
-                    updated = parse_gh_datetime(updated)
-                    created = parse_gh_datetime(repository["created_at"])
-
-                    yield {
-                        "url": repository["url"],
-                        "name": repository["name"],
-                        "languages": [repository["language"]],
-                        "score": self.calculate_score(updated, repository),
-                        "summary": repository.get("description", ""),
-                        "updated_at": updated,
-                        "created_at": created
-                    }
+            for lang in langs:
+                for repository in self.iter_for_lang(lang):
+                    yield repository
